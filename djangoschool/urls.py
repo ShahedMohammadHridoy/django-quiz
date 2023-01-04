57aaa3c1e137051d4443fe5617b1499a7b126766
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-01-04 19:28:23 +0600

    project setup

--- djangoschool/urls.py
+++ djangoschool/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', include('school.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', classroom.SignUpView.as_view() , name='signup'),
-    # path('accounts/signup/student/', classroom.),
-    # path('accounts/signup/teacher/', classroom.),
+    path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
+    path('accounts/signup/teacher/', teachers.TeacherSignUpView.as_view() , name='teacher_signup'),
 ]
